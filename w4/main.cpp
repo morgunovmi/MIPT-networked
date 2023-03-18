023d495f38ac03f2590b8f8aec69a0990d032012
Max <morgunov.mi@phystech.edu>
2023-03-18 22:35:16 +0300

    Fix mem leak

--- w4/main.cpp
+++ w4/main.cpp
@@ -118,6 +118,7 @@ int main(int argc, const char **argv)
           on_snapshot(event.packet);
           break;
         };
+        enet_packet_destroy(event.packet);
         break;
       default:
         break;
